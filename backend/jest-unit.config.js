a4ab5d5b491d85cbd8997d4d6f3d24fad5b15cc1
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2023-04-29 19:40:04 -0300

    chore: update jest config

--- backend/jest-unit.config.js
+++ backend/jest-unit.config.js
@@ -20,7 +20,13 @@ module.exports = {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/domain/dto/**',
+    '!<rootDir>/src/domain/models/**',
+    '!<rootDir>/src/domain/repositories/**',
+  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
