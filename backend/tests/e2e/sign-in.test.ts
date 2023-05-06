3c5ac7cc2d0defaa5138d612921cf098493325f9
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2023-05-06 15:11:35 -0300

    test: create sign-in route test suite

--- backend/tests/e2e/sign-in.test.ts
+++ backend/tests/e2e/sign-in.test.ts
@@ -7,6 +7,12 @@ import server from '../../src/main/config/server';
 
 import { UserDTO } from '../../src/domain/dto';
 
+import {
+  MissingBodyParamsError,
+  UserDoesNotExistsError,
+  PasswordDoesNotMatchError,
+} from '../../src/app/errors';
+
 import { DatabaseTransaction } from '../../src/app/contracts/database';
 
 import { PostgresTransaction } from '../../src/infra/database/postgres';
@@ -63,6 +69,78 @@ describe('POST - api/V1/sign-in', () => {
 
     expect(response.status).toBe(200);
     expect(response.body).toHaveProperty('accessToken');
+  });
+
+  it('should return 400 if email is empty', async () => {
+    const account = {
+      email: '',
+      password: '12345678xX@',
+    };
+
+    const response = await signInRequest(account.email, account.password);
+
+    const error = new MissingBodyParamsError(['email']);
+
+    expect(response.status).toBe(400);
+    expect(response.body).toStrictEqual({
+      message: error.message,
+      name: error.name,
+    });
+  });
+
+  it('should return 400 if password is empty', async () => {
+    const account = {
+      email: 'test@mail.com',
+      password: '',
+    };
+
+    const response = await signInRequest(account.email, account.password);
+
+    const error = new MissingBodyParamsError(['password']);
+
+    expect(response.status).toBe(400);
+    expect(response.body).toStrictEqual({
+      message: error.message,
+      name: error.name,
+    });
+  });
+
+  it('should return 401 if user does not exists', async () => {
+    const account = {
+      email: 'test@mail.com',
+      password: '12345678xX@',
+    };
+
+    const response = await signInRequest(account.email, account.password);
+
+    const error = new UserDoesNotExistsError();
+
+    expect(response.status).toBe(401);
+    expect(response.body).toStrictEqual({
+      message: error.message,
+      name: error.name,
+    });
+  });
+
+  it('should return 403 if user password does not match', async () => {
+    const user = {
+      email: 'test@mail.com',
+      password: '12345678xX@',
+      name: 'test',
+      lastname: 'test',
+    };
+    const otherPassword = '12345678xX$';
+
+    await signUpRequest(user);
+    const response = await signInRequest(user.email, otherPassword);
+
+    const error = new PasswordDoesNotMatchError();
+
+    expect(response.status).toBe(403);
+    expect(response.body).toStrictEqual({
+      message: error.message,
+      name: error.name,
+    });
   });
 
   afterEach(async () => {
