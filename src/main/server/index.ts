bdab43262a57c28c129c88598ab16f85f20b5c31
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2023-05-20 20:05:48 -0300

    feat: add catch error in server init

--- src/main/server/index.ts
+++ src/main/server/index.ts
@@ -5,8 +5,12 @@ import '../bootstrap';
 import { loader } from '../loaders';
 import server from '../config/server';
 
-loader().then(async () => {
-  (await server()).listen(Number(process.env.PORT), () => {
-    console.log(`server running at port: ${Number(process.env.PORT)}`);
+loader()
+  .then(async () => {
+    (await server()).listen(Number(process.env.PORT), () => {
+      console.log(`server running at port: ${Number(process.env.PORT)}`);
+    });
+  })
+  .catch(error => {
+    console.log(`error in running server error below:\n${error}`);
   });
-});
